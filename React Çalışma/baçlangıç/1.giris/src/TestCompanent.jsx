1be08948401d64e975e9491a0ba466f42b1400ad
mrtyilmaz1 <muryilmaz95@gmail.com>
2024-06-12 12:58:40 +0300

    ders 3
    Enes Bayram Youtube

---
+++ React Çalışma/baçlangıç/1.giris/src/TestCompanent.jsx
@@ -0,0 +1,10 @@
+// rfce yazarak hazır kompanent çağırılır.
+
+
+function TestCompanent() {
+  return (
+    <div>TestCompanent</div>
+  )
+}
+
+export default TestCompanent
--- React Çalışma/baçlangıç/1.giris/src/main.jsx
+++ React Çalışma/baçlangıç/1.giris/src/main.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')). // index.html'de tanımlanan root yakalanır.
+render( // yakalayınca render et. App kompanentini render eder. App.jsx'de return içindeki değeri dönderir.
   <React.StrictMode>
-    <App />
+    <App /> 
   </React.StrictMode>,
 )
