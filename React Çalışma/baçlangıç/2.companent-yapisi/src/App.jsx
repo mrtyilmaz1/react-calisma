e3c7544b03b95ae734de9efb3067dbc7ba6fdbda
mrtyilmaz1 <muryilmaz95@gmail.com>
2024-06-12 16:17:05 +0300

    companent kullanımı

---
+++ React Çalışma/baçlangıç/2.companent-yapisi/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+// import Login from './Login'
+
+function App() {
+  // return <div><Login /></div>;
+  return (
+
+    <div>Murat</div>
+  )
+
+
+
+}
+
+export default App;
---
+++ React Çalışma/baçlangıç/2.companent-yapisi/src/Login.jsx
@@ -0,0 +1,27 @@
+
+function Login() {
+  return (
+
+    //Fragment <></> kapsayıcı olarak kullanılır. Kapsayıcı div diyebiliriz.
+
+    <div>
+      <div>
+        <p>Kullanıcı Adı:</p>
+        <input type="text" />
+      </div>
+      <div>
+        <p>Şifreniz:</p>
+        <input type="password" />
+
+      </div>
+      <hr />
+      <button>Giriş Yap</button>
+    </div>
+
+
+
+
+  )
+}
+
+export default Login
