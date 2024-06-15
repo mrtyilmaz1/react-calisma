86d5c92a5d6ee43400a1ef0b43419ce7e17bd428
mrtyilmaz1 <muryilmaz95@gmail.com>
2024-06-15 22:55:17 +0300

    ilk proje ve header'ı

---
+++ React Çalışma/baçlangıç/6.course-app/src/Header.jsx
@@ -0,0 +1,10 @@
+import './css/Header.css'
+function Header() {
+    return (
+        <div className="header">
+            <div className='title'>KURSLARIM</div>
+        </div>
+    )
+}
+
+export default Header
