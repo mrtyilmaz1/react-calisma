f91fd4fceb6c45d5637f88d5648925a3aecac4d1
mrtyilmaz1 <muryilmaz95@gmail.com>
2024-07-06 18:56:41 +0300

    fix

--- React Çalışma/baçlangıç/12.E-COMMERCE/src/App.jsx
+++ React Çalışma/baçlangıç/12.E-COMMERCE/src/App.jsx
@@ -1,7 +1,8 @@
 
 import './App.css'
 import Header from './components/Header'
-import ProductList from './components/ProductList'
+import Loading from './components/Loading'
+import RouterConfig from './config/RouterConfig'
 import PageContainer from './container/PageContainer'
 
 
@@ -11,10 +12,9 @@ function App() {
     <div>
 
       <PageContainer >
-
+        <Loading />
         <Header />
-        <ProductList />
-
+        <RouterConfig />
       </PageContainer>
 
     </div>
